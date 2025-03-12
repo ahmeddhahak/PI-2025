34e3a5868f256f3b2a91c6d3c9461f81c5696469
Ahmed Dhahak <163906314+ahmeddhahak@users.noreply.github.com>
2025-03-12 18:29:29 +0100

    Init

---
+++ care4elders/src/test/java/tn/esprit/care4elders/Care4eldersApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.care4elders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Care4eldersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
